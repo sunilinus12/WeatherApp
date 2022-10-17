d225973fb59036760891b245dd970eda9f14983f
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-10-17 18:22:22 +0530

    added the down section and top sections and status

--- App.js
+++ App.js
@@ -7,14 +7,33 @@ import {
   useWindowDimensions,
   View,
   Animated,
+  TouchableOpacity,
 } from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import Search from './assets/search.svg';
+import Menu from './assets/menu.svg';
 import React, {useEffect, useRef} from 'react';
 import Locations from './utils/Locations';
+import Sun from './assets/sun.svg';
+import Moon from './assets/moon.svg';
+import Rain from './assets/rain.svg';
+import Cloudy from './assets/cloudy.svg';
 
 export default function App() {
   const {width, height} = useWindowDimensions();
   const X = useRef(new Animated.Value(0)).current;
 
+  const handleWeatherType = i => {
+    if (i === 'Sunny') {
+      return <Sun width={34} height={34} fill={'white'} />;
+    } else if (i === 'Night') {
+      return <Moon width={34} height={34} fill={'white'} />;
+    } else if (i === 'Cloudy') {
+      return <Cloudy width={34} height={34} fill={'white'} />;
+    } else if (i === 'Rainy') {
+      return <Rain width={34} height={34} fill={'white'} />;
+    }
+  };
   const handleBackImg = i => {
     if (i === 'Sunny') {
       return require('./assets/sunny.jpg');
@@ -30,6 +49,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle={'light-content'} />
+
       <ScrollView
         horizontal={true}
         pagingEnabled
@@ -60,21 +80,78 @@ export default function App() {
                     flex: 1,
                     backgroundColor: 'rgba(0,0,0,0.4)',
                     padding: 20,
-                    justifyContent: 'center',
-                    alignItems: 'center',
                   }}>
-                  <Text
+                  <View style={styles.topinfowrapper}>
+                    <View>
+                      <Text style={styles.city}>{item.city}</Text>
+                      <Text style={styles.time}>{item.dateTime}</Text>
+                    </View>
+                    <View>
+                      <Text style={styles.temp}>{item.temparature}</Text>
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                        }}>
+                        {handleWeatherType(item.weatherType)}
+                        <Text style={styles.type}>{item.weatherType}</Text>
+                      </View>
+                    </View>
+                  </View>
+
+                  <View
                     style={{
-                      color: 'white',
-                    }}>
-                    {item.city}
-                  </Text>
+                      marginTop: 20,
+                      borderBottomColor: 'gray',
+                      borderBottomWidth: 1,
+                    }}
+                  />
+                  <View style={styles.bottominfowrapper}>
+                    <View
+                      style={{
+                        alignItems: 'center',
+                      }}>
+                      <Text style={styles.infotext}>wind</Text>
+                      <Text style={[styles.infotext, {fontSize: 24}]}>
+                        {item.wind}
+                      </Text>
+                      <Text style={styles.infotext}>km/h</Text>
+                    </View>
+                    <View
+                      style={{
+                        alignItems: 'center',
+                      }}>
+                      <Text style={styles.infotext}>Rain</Text>
+                      <Text style={[styles.infotext, {fontSize: 24}]}>
+                        {item.rain}
+                      </Text>
+                      <Text style={styles.infotext}>%</Text>
+                    </View>
+                    <View
+                      style={{
+                        alignItems: 'center',
+                      }}>
+                      <Text style={styles.infotext}>humidity</Text>
+                      <Text style={[styles.infotext, {fontSize: 24}]}>
+                        {item.humidity}
+                      </Text>
+                      <Text style={styles.infotext}>%</Text>
+                    </View>
+                  </View>
                 </View>
               </ImageBackground>
             </View>
           );
         })}
       </ScrollView>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Search width={24} height={24} fill={'white'} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Menu width={24} height={24} fill={'white'} />
+        </TouchableOpacity>
+      </View>
       <View
         style={{
           position: 'absolute',
@@ -109,4 +186,50 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  infotext: {
+    fontSize: 14,
+    color: 'white',
+    fontFamily: 'Montserrat-Regular',
+  },
+  topinfowrapper: {
+    flex: 1,
+    marginTop: 160,
+    justifyContent: 'space-between',
+  },
+  city: {
+    fontSize: 30,
+    color: 'white',
+    fontFamily: 'Montserrat-Medium',
+  },
+  time: {
+    color: 'white',
+    fontFamily: 'Montserrat-Medium',
+  },
+  temp: {
+    color: '#fff',
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 85,
+  },
+  type: {
+    color: 'white',
+    lineHeight: 34,
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 24,
+    marginLeft: 10,
+  },
+  bottominfowrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    height: getStatusBarHeight() + 40,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+});
