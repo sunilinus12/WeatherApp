30c6bc314ee548e31cde967f311af87bb656e8c4
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-10-17 15:20:49 +0530

    added the scollview indicator

--- App.js
+++ App.js
@@ -6,12 +6,14 @@ import {
   Text,
   useWindowDimensions,
   View,
+  Animated,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Locations from './utils/Locations';
 
 export default function App() {
   const {width, height} = useWindowDimensions();
+  const X = useRef(new Animated.Value(0)).current;
 
   const handleBackImg = i => {
     if (i === 'Sunny') {
@@ -24,25 +26,85 @@ export default function App() {
       return require('./assets/rainy.jpg');
     }
   };
+
   return (
     <>
       <StatusBar barStyle={'light-content'} />
       <ScrollView
         horizontal={true}
         pagingEnabled
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: X,
+                },
+              },
+            },
+          ],
+          {
+            useNativeDriver: false,
+          },
+        )}
+        scrollEventThrottle={1}
         showsHorizontalScrollIndicator={false}>
         {Locations.map((item, key) => {
           let bgimg = handleBackImg(item.weatherType);
-          console.log(bgimg);
+
           return (
             <View key={key} style={{width: width, height: height}}>
-              <ImageBackground
-                style={{flex: 1}}
-                source={bgimg}></ImageBackground>
+              <ImageBackground style={{flex: 1}} source={bgimg}>
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: 'rgba(0,0,0.3',
+                    padding: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      color: 'white',
+                    }}>
+                    {item.city}
+                  </Text>
+                </View>
+              </ImageBackground>
             </View>
           );
         })}
       </ScrollView>
+      <View
+        style={{
+          position: 'absolute',
+          top: 160,
+          left: 20,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {Locations.map((item, key) => {
+          const width2 = X.interpolate({
+            inputRange: [width * (key - 1), width * key, width * (key + 1)],
+            outputRange: [5, 12, 5],
+            extrapolate: 'clamp',
+          });
+          return (
+            <Animated.View
+              key={key}
+              style={{
+                height: 5,
+                width: 5,
+                borderRadius: 4,
+                backgroundColor: 'white',
+                marginHorizontal: 4,
+                width: width2,
+              }}
+            />
+          );
+        })}
+      </View>
     </>
   );
 }
